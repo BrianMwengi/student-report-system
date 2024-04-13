20797746f05fff3ca6fc0fb0b69e7ced79fcb64f
Brian Mwengi <mwengibrian@gmail.com>
2024-04-13 08:11:54 +0300

    Add Route For classforms

---
+++ app/Http/Controllers/ClassFormController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ClassFormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function index(): View
+    {
+        return view('classforms', [
+            //
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClassFormController;
 
 Route::view('/', 'welcome');
+
+Route::get('classforms', [ClassFormController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('classforms'); 
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
