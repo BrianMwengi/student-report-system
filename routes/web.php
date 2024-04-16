f31e5ef713a39eb670eaee6d8cc9e1a1405a1481
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 15:22:01 +0300

    Add Subjects Route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubjectController;
 use App\Http\Controllers\ClassFormController;
 
 Route::view('/', 'welcome');
@@ -8,6 +9,10 @@ Route::view('/', 'welcome');
 Route::get('classforms', [ClassFormController::class, 'index'])
     ->middleware(['auth', 'verified'])
     ->name('classforms'); 
+
+Route::get('subjects', [SubjectController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('subjects'); 
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
