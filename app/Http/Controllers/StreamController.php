15e835e5e4de8efa73f036cdca3cbfebd81d03fe
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 16:17:42 +0300

    Modify the StreamController to add the following code to the store method:

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ClassForm;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
@@ -10,7 +11,8 @@ class StreamController extends Controller
     public function index(): View
     {
         return view('streams', [
-            //
+            $classes = ClassForm::all(),
+            ['classes' => $classes],
         ]);
     }
 }
--- resources/views/livewire/streams/create.blade.php
+++ resources/views/livewire/streams/create.blade.php
@@ -1,9 +1,26 @@
 <?php
 
 use Livewire\Volt\Component;
+use Livewire\Attributes\Validate;
+use App\Models\Stream;
 
 new class extends Component {
-    //
+    #[Validate('required|string|max:255')]
+    public $name = '';
+    #[Validate('required|exists:classes,id')]
+    public $class_id ='';
+
+    public function submit()
+    {
+        // Validation rules
+        $validatedData = $this->validate();
+
+        // Create a new stream
+        Stream::create($validatedData);
+
+        // Show a success message or redirect to another page
+        $this->dispatch('success', message: "Stream added successfully!");
+    }
 }; ?>
 
 <div>
