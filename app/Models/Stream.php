f43324f5244833d5170d9d9ac6d1ed92a954a683
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 15:48:46 +0300

    Add Stream Model

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\ClassForm;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Stream extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'class_id',
+    ];
+
+    public function classForm()
+    {
+        return $this->belongsTo(ClassForm::class, 'class_id');
+    }
+
 }
--- database/migrations/2024_04_16_122647_create_streams_table.php
+++ database/migrations/2024_04_16_122647_create_streams_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedBigInteger('class_id');
-            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
+            $table->foreign('class_id')->references('id')->on('class_forms')->onDelete('cascade');
             $table->timestamps();
         });
     }
