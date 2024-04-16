9b5ea9ed90c8768b1aec54ee5d8692033b29d2a9
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 15:52:25 +0300

    Update StreamController With Index method

--- app/Http/Controllers/StreamController.php
+++ app/Http/Controllers/StreamController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class StreamController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('streams', [
+            //
+        ]);
+    }
 }
