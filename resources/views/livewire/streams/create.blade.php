d68d5ad40c9c54a9926750b414cf3fd103dd64cc
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 16:20:01 +0300

    Update create.blade.php

--- resources/views/livewire/streams/create.blade.php
+++ resources/views/livewire/streams/create.blade.php
@@ -24,5 +24,37 @@ new class extends Component {
 }; ?>
 
 <div>
-    //
+    <div>
+        <h2 class="text-xl font-bold">Add Stream</h2>
+    
+        <form wire:submit ="submit" class="mt-4">
+            <div class="form-group">
+                <input type="text" class="form-input @error('name') border-red-500 @enderror" wire:model="name" placeholder="Stream Name">
+    
+                @error('name')
+                <div class="text-red-500 mt-1">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+    
+            <div class="form-group mt-4">
+                <select class="form-select @error('class_id') border-red-500 @enderror" wire:model="class_id">
+                    <option value="">Select Class</option>
+                    @foreach ($classes as $class)
+                        <option value="{{ $class->id }}">{{ $class->name }}</option>
+                    @endforeach
+                </select>
+    
+                @error('class_id')
+                <div class="text-red-500 mt-1">
+                    {{ $message }}
+                </div>
+                @enderror
+            </div>
+    
+            <button type="submit" class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+        </form>
+    </div>
+    
 </div>
