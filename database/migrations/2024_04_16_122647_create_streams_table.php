cbb66cf1bf78f2994008626362c3f61e8e59c937
Brian Mwengi <mwengibrian@gmail.com>
2024-04-16 15:33:48 +0300

    Add Streams Table Fields

--- database/migrations/2024_04_16_122647_create_streams_table.php
+++ database/migrations/2024_04_16_122647_create_streams_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('streams', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('class_id');
+            $table->foreign('class_id')->references('id')->on('classes')->onDelete('cascade');
             $table->timestamps();
         });
     }
