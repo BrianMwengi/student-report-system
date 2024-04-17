d7748557f001edf209f89ef4c6dcc683343d1106
Brian Mwengi <mwengibrian@gmail.com>
2024-04-17 09:27:51 +0300

    Update the streams.blade.php file

--- resources/views/streams.blade.php
+++ resources/views/streams.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
-    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        <livewire:streams.create />
+    <div class="container mt-5">
+        <div class="max-w-lg mx-auto">
+            <livewire:streams.create />
+        </div>
     </div>
 </x-app-layout>
 
