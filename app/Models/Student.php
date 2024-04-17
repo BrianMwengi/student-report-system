4c2a22aeea2bcf183cb190b210037b1b65caa186
Brian Mwengi <mwengibrian@gmail.com>
2024-04-17 12:21:24 +0300

    Update student model

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Stream;
+use App\Models\ClassForm;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Student extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'adm_no',
+        'term',
+        'form',
+        'stream_id',
+        'form_sequence_number',    
+    ];
+
+    public function stream(): BelongsTo
+    {
+        return $this->belongsTo(Stream::class);
+    }
+
+    public function form(): BelongsTo
+    {
+        return $this->belongsTo(ClassForm::class, 'form', 'id');
+    }
 }
+
