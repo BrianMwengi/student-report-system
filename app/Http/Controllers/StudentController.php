642995a1eeb7dcf994b0026b6d32148c23c7f881
Brian Mwengi <mwengibrian@gmail.com>
2024-04-17 12:32:15 +0300

    Add A return statement to the index method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('students', [
+            //
+        ]);
+    }
 }
