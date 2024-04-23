99bba6c84e15419c75f2ba8735567d9e57864b92
Brian Mwengi <mwengibrian@gmail.com>
2024-04-23 16:46:42 +0300

    Add livewire component studentdetails & modified studentdetails blade file

---
+++ resources/views/studentdetails.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="container mt-5">
+        <div class="max-w-lg mx-auto">
+            <livewire:studentdetails.create />
+        </div>
+    </div>
+</x-app-layout>
+
