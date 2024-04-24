c986262b3bb42a73f25dd5dcce4413b17f2b5f91
Brian Mwengi <mwengibrian@gmail.com>
2024-04-24 17:12:20 +0300

    Update create.blade.php with flash message

--- resources/views/livewire/studentdetails/create.blade.php
+++ resources/views/livewire/studentdetails/create.blade.php
@@ -42,11 +42,47 @@ new class extends Component {
             ]
         );
 
-        // Show a success message or redirect to another page
-        session()->flash('message', 'Student details saved successfully.');
+         // Show a success message or redirect to another page
+         $this->dispatch('success', message: "Student detail added successfully!");
     }
 }; ?>
 
 <div>
-    //
+    <div class="container mt-5">
+        <h2 class="mb-4">Add/Edit Student Primary School-Details</h2>
+        <form wire:submit.prevent="submit" class="needs-validation" novalidate>
+            <div class="mb-3">
+                <input type="text" wire:model="name" class="form-input" placeholder="Student Name">
+                @error('name') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" wire:model="adm_no" class="form-input" placeholder="Admission Number">
+                @error('adm_no') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" wire:model="primary_school" class="form-input" placeholder="Primary School" value="{{ $primary_school }}">
+                @error('primary_school') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" wire:model="kcpe_year" class="form-input" placeholder="KCPE Year" value="{{ $kcpe_year }}">
+                @error('kcpe_year') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" wire:model="kcpe_marks" class="form-input" placeholder="KCPE Marks" value="{{ $kcpe_marks }}">
+                @error('kcpe_marks') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <div class="mb-3">
+                <input type="text" wire:model="kcpe_position" class="form-input" placeholder="KCPE Position" value="{{ $kcpe_position }}">
+                @error('kcpe_position') <div class="text-red-500">{{ $message }}</div> @enderror
+            </div>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+        </form>
+        <div x-data="{ open: false, message: '' }" 
+             x-cloak
+        @success.window="open = true; message = $event.detail.message; setTimeout(() => open = false, 4000)"
+        x-show="open"
+        class="mt-4 bg-green-500 text-white font-bold py-2 px-4 rounded">
+        <span x-text="message"></span>
+</div>
+    </div>    
 </div>
