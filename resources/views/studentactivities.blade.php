9b83f5a73cef618cfbdc7cb7bae0b132dbf0358f
Brian Mwengi <mwengibrian@gmail.com>
2024-04-27 00:06:08 +0300

    Updated student activities page

---
+++ resources/views/studentactivities.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="container mt-5">
+        <div class="max-w-lg mx-auto">
+            <livewire:studentactivities.create />
+        </div>
+    </div>
+</x-app-layout>
+
