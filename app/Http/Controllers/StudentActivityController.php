fef24ed274ae7a1292b57fd83f9cb384245ffb23
Brian Mwengi <mwengibrian@gmail.com>
2024-04-27 00:01:08 +0300

    Update StudentActivityController.php

--- app/Http/Controllers/StudentActivityController.php
+++ app/Http/Controllers/StudentActivityController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class StudentActivityController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('studentactivities', [
+            //
+        ]);
+    }  
 }
