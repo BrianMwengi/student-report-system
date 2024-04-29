d79beeb37c5ad89d9cca5dca5fb8eb4a0bd052b2
Brian Mwengi <mwengibrian@gmail.com>
2024-04-29 14:12:52 +0300

    Add blade code to display the school settings

---
+++ resources/views/schoolsettings.blade.php
@@ -0,0 +1,8 @@
+<x-app-layout>
+    <div class="container mt-5">
+        <div class="max-w-lg mx-auto">
+            <livewire:schoolsettings.create />
+        </div>
+    </div>
+</x-app-layout>
+
