0f76d84ad266e8b404cb70ce1b36f083cc0c2e4b
Brian Mwengi <mwengibrian@gmail.com>
2024-04-29 14:07:59 +0300

    Update school settings controller

--- app/Http/Controllers/SchoolSettingsController.php
+++ app/Http/Controllers/SchoolSettingsController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class SchoolSettingsController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('schoolsettings', [
+            //
+        ]);
+    }  
+
 }
