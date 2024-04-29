24901e5257595d80601d52e030ca31b37f290ecb
Brian Mwengi <mwengibrian@gmail.com>
2024-04-29 14:03:27 +0300

    Update SchoolSettings.php with protected $fillable

--- app/Models/SchoolSettings.php
+++ app/Models/SchoolSettings.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class SchoolSettings extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'id',
+        'school_name', 
+        'current_year',
+        'term_start_date', 
+        'next_term_start_date', 
+        'next_term_end_date', 
+        'term_end_date', 
+        'term', 
+        'school_motto', 
+        'school_vision', 
+        'logo_url'
+    ];
 }
