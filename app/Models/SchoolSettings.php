5dcaaa7c96e19537af659a905b9eb440fe5a9d08
Brian Mwengi <mwengibrian@gmail.com>
2024-04-29 12:50:18 +0300

    Add new files to the repository

---
+++ app/Models/SchoolSettings.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SchoolSettings extends Model
+{
+    use HasFactory;
+}
