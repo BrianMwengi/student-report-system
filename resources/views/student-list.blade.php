ab5920c0045e4e491548876befc7882bcfbb2af9
Brian Mwengi <mwengibrian@gmail.com>
2024-05-01 14:13:51 +0300

    Add new file resources/views/livewire/student-list/list.blade.php and modified resources/views/student-list.blade.php

--- resources/views/student-list.blade.php
+++ resources/views/student-list.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container mt-5">
         <div class="max-w-lg mx-auto">
-            <livewire:student-list.create />
+            <livewire:student-list.list />
         </div>
     </div>
 </x-app-layout>
