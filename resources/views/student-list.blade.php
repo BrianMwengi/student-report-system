b3062733cd0027b9f1bcf6c807cde0670a5b9836
Brian Mwengi <mwengibrian@gmail.com>
2024-05-01 14:16:37 +0300

    Update student list view

--- resources/views/student-list.blade.php
+++ resources/views/student-list.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <div class="container mt-5">
         <div class="max-w-lg mx-auto">
-            <livewire:student-list.list />
+            <livewire:student.list />
         </div>
     </div>
 </x-app-layout>
