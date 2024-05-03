5750929d44973ff88110f5061a057f83d00e49e0
Brian Mwengi <mwengibrian@gmail.com>
2024-05-03 13:31:47 +0300

    Update delete.blade.php

--- resources/views/livewire/students/delete.blade.php
+++ resources/views/livewire/students/delete.blade.php
@@ -26,5 +26,22 @@ new class extends Component {
 }; ?>
 
 <div>
-    //
+    <script>
+        window.addEventListener('show-delete-confirmation', function(event) {
+            let id = event.detail.id;
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    window.livewire.emit('deleteStudent', id);
+                }
+            });
+        });
+    </script>
 </div>
