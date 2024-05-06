300ba167c36f4e57a477937b810a25b51cfdc8a6
Brian Mwengi <mwengibrian@gmail.com>
2024-05-06 14:30:17 +0300

    Update StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,19 +7,10 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    public function index($view = 'list', $id = null): View
+    public function index(): View
     {
-        // If an ID is provided, show the edit view
-        if ($view === 'edit' && $id) {
-            return view('students', ['view' => 'edit', 'id' => $id]);
-        }
-
-        // If view is 'create', show the create view
-        if ($view === 'create') {
-            return view('students', ['view' => 'create']);
-        }
-
-        // Otherwise, show the list view
-        return view('students', ['view' => 'list']);
+        return view('students', [
+            //
+        ]);
     }
 }
