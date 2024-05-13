29a49c4bf6ebcab2235a0df0ad902360d110ab9c
Brian Mwengi <mwengibrian@gmail.com>
2024-05-14 00:47:52 +0300

    Updated reports.blade.php

---
+++ resources/views/reports.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="container mt-5">
+        <div class="max-w-lg mx-auto">
+            <livewire:reports.reportcard />
+        </div>
+    </div>
+</x-app-layout>
+
+
+
