7bcd540225bbcbe273a4749620856665f3c9dbee
Brian Mwengi <mwengibrian@gmail.com>
2024-05-14 00:56:02 +0300

    Update ReportCardController.php

--- app/Http/Controllers/ReportCardController.php
+++ app/Http/Controllers/ReportCardController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class ReportCardController extends Controller
 {
-    //
+    public function index($studentId): View
+    {
+        return view('report-card', ['selectedStudentId' => $studentId]);
+    }
 }
