0afd2863983c8055d644e95c6195ba233d9f4dc6
Brian Mwengi <mwengibrian@gmail.com>
2024-07-25 13:30:27 +0300

    Update the edit component

--- resources/views/livewire/students/edit.blade.php
+++ resources/views/livewire/students/edit.blade.php
@@ -40,7 +40,7 @@ new class extends Component {
     public $examId;
     public $form_sequence_number;
     public Student $student;
-    
+
     // Initialize the component
     public function mount($id): void
     {
@@ -48,25 +48,46 @@ new class extends Component {
         $this->student = Student::find($id);
         // if the student is found, set the properties
         if ($this->student) {
-            $this->studentId = $this->student->id;
-            $this->form_sequence_number = $this->student->form_sequence_number;
-            $this->name = $this->student->name;
-            $this->adm_no = $this->student->adm_no;
-            $this->form = $this->student->form; 
-            $this->stream_id = $this->student->stream_id;
-            
-            // Get the student details
-            $this->studentDetailsId = $this->student->details;
-            $this->studentDetails = $this->student->details;
-            // if the student details are found, set the properties
-            if ($this->studentDetails) {
-                $this->primary_school = $this->studentDetails->primary_school;
-                $this->kcpe_marks = $this->studentDetails->kcpe_marks;
-                $this->kcpe_year = $this->studentDetails->kcpe_year;
-                $this->kcpe_position = $this->studentDetails->kcpe_position;
-            }
+            $this->setStudentProperties();
+            $this->setStudentDetails();
+            $this->setExamDetails();
+        }
 
-        // Find the exam details for the student    
+        // Get all the classes, streams and subjects
+        $this->classes = ClassForm::all();
+        $this->streams = Stream::all();
+        $this->subjects = Subject::all();
+        $this->subject_id = $this->subjects->first()->id;
+    }
+
+    // Set student properties
+    private function setStudentProperties(): void
+    {
+        $this->studentId = $this->student->id;
+        $this->form_sequence_number = $this->student->form_sequence_number;
+        $this->name = $this->student->name;
+        $this->adm_no = $this->student->adm_no;
+        $this->form = $this->student->form;
+        $this->stream_id = $this->student->stream_id;
+    }
+
+    // Set student details
+    private function setStudentDetails(): void
+    {
+        $this->studentDetailsId = $this->student->details;
+        $this->studentDetails = $this->student->details;
+        // if the student details are found, set the properties
+        if ($this->studentDetails) {
+            $this->primary_school = $this->studentDetails->primary_school;
+            $this->kcpe_marks = $this->studentDetails->kcpe_marks;
+            $this->kcpe_year = $this->studentDetails->kcpe_year;
+            $this->kcpe_position = $this->studentDetails->kcpe_position;
+        }
+    }
+
+    // To fetch and set the exam details for a student when the component is mounted
+    private function setExamDetails(): void
+    {
         $exam = Exam::where('student_id', $this->studentId)->first();
         if ($exam) {
             $this->exam1 = $exam->exam1;
@@ -78,83 +99,66 @@ new class extends Component {
         }
     }
 
-        // Get all the classes, streams and subjects
-        $this->classes = ClassForm::all();
-        $this->streams = Stream::all();
-        $this->subjects = Subject::all();
-        $this->subject_id = $this->subjects->first()->id;
-    }
-
     // Method to update the student details
     public function updateStudent(): void
-    { 
+    {
         $validatedData = $this->validate();
         // If the student ID is set, update the student details
         if ($this->studentId) {
             $student = Student::find($this->studentId);
             $student->update($validatedData);
-            
-            // Find the student details
-            $studentDetails = StudentDetail::where('student_id', $this->studentId)->first();
-            // If the student details are found, update the details
-            if ($studentDetails) {
-                // empty array to hold the KCPE details
-                $kcpeDetails = [];
-                if ($this->primary_school) {
-                    $kcpeDetails['primary_school'] = $this->primary_school;
-                }
-                if ($this->kcpe_marks) {
-                    $kcpeDetails['kcpe_marks'] = $this->kcpe_marks;
-                }
-                if ($this->kcpe_year) {
-                    $kcpeDetails['kcpe_year'] = $this->kcpe_year;
-                }
-                if ($this->kcpe_position) {
-                    $kcpeDetails['kcpe_position'] = $this->kcpe_position;
-                }
-                // If the KCPE details are not empty, update the student details
-                if (!empty($kcpeDetails)) {
-                    $studentDetails->update($kcpeDetails);
-                }
-            }
-    
-            // Update the Exam model
-            Exam::updateOrCreate(
-                // Find the exam details for the student
-                ['id' => $this->examId, 'student_id' => $this->studentId, 'subject_id' => $this->subject_id],
-                [
-                    'subject_id' => $this->subject_id,
-                    'exam1' => $this->exam1,
-                    'exam2' => $this->exam2,
-                    'exam3' => $this->exam3,
-                    'teacher' => $this->teacher,
-                ]
-            );
-    
-            // If the update was successful, flash a success message
+            $this->updateStudentDetails();
+            $this->updateExam();
             session()->flash('success', 'Student updated successfully');
-            // Dispatch an event to update the student list
-            $this->dispatch('student-updated');
-    
         } else {
             session()->flash('error', 'Failed to update student details.');
         }
     }
-    
+
+    // Update student details
+    private function updateStudentDetails(): void
+    {
+        $studentDetails = StudentDetail::where('student_id', $this->studentId)->first();
+        // If the student details are found, update the details
+        if ($studentDetails) {
+            $kcpeDetails = array_filter([
+                'primary_school' => $this->primary_school,
+                'kcpe_marks' => $this->kcpe_marks,
+                'kcpe_year' => $this->kcpe_year,
+                'kcpe_position' => $this->kcpe_position,
+            ]);
+            if (!empty($kcpeDetails)) {
+                $studentDetails->update($kcpeDetails);
+            }
+        }
+    }
+
+    // Update or create exam details 
+    private function updateExam(): void
+    {
+        Exam::updateOrCreate(
+            // Find the exam details for the student
+            ['id' => $this->examId, 'student_id' => $this->studentId, 'subject_id' => $this->subject_id],
+            [
+                'subject_id' => $this->subject_id,
+                'exam1' => $this->exam1,
+                'exam2' => $this->exam2,
+                'exam3' => $this->exam3,
+                'teacher' => $this->teacher,
+            ]
+        );
+    }
+
     // Method to fetch the exam details for the student
     public function updatedSubjectId()
-    {  
+    {
         // If the subject ID is set, get the exam details
         if ($this->subject_id) {
             // Find the exam details for the student
             $exam = Exam::where('student_id', $this->studentId)->where('subject_id', $this->subject_id)->first();
             // If the exam details are found, set the properties
             if ($exam) {
-                $this->exam1 = $exam->exam1;
-                $this->exam2 = $exam->exam2;
-                $this->exam3 = $exam->exam3;
-                $this->teacher = $exam->teacher;
-                $this->examId = $exam->id;
+                $this->setExamProperties($exam);
             } else {
                 $this->resetExamFields();
             }
@@ -163,8 +167,18 @@ new class extends Component {
         }
     }
 
+    // Set exam properties
+    private function setExamProperties($exam): void
+    {
+        $this->exam1 = $exam->exam1;
+        $this->exam2 = $exam->exam2;
+        $this->exam3 = $exam->exam3;
+        $this->teacher = $exam->teacher;
+        $this->examId = $exam->id;
+    }
+
     // Reset the exam fields
-    private function resetExamFields()
+    private function resetExamFields(): void
     {
         $this->exam1 = '';
         $this->exam2 = '';
@@ -172,7 +186,7 @@ new class extends Component {
         $this->teacher = '';
         $this->examId = null;
     }
-    }; ?>
+}; ?>
     <div>
        <form class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             <div class="mb-3">
