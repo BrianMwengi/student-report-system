7d9df9b94c770e07099da5d802547edf7b12e011
Brian Mwengi <mwengibrian@gmail.com>
2024-08-07 11:06:22 +0300

    Update ExamFactory.php with comments

--- database/factories/ExamFactory.php
+++ database/factories/ExamFactory.php
@@ -24,14 +24,26 @@ class ExamFactory extends Factory
      *
      * @return array
      */
+    
     public function definition()
     {
         return [
+            // Randomly select a student ID from the Student model
             'student_id' => Student::all()->random()->id,
+            
+            // Randomly select a subject ID from the Subject model
             'subject_id' => Subject::all()->random()->id,
+            
+            // Generate a random number between 0 and 30 for exam1
             'exam1' => $this->faker->numberBetween($min = 0, $max = 30),
+            
+            // Generate a random number between 0 and 30 for exam2
             'exam2' => $this->faker->numberBetween($min = 0, $max = 30),
+            
+            // Generate a random number between 0 and 70 for exam3
             'exam3' => $this->faker->numberBetween($min = 0, $max = 70),
+            
+            // Generate a random name for the teacher
             'teacher' => $this->faker->name,
         ];
     }
