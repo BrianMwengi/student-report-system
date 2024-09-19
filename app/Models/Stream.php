9b2f75e212d57ea651e17e8c292a73bcf419b37b
Brian Mwengi <mwengibrian@gmail.com>
2024-09-19 11:46:50 +0300

    Update Stream model with BelongsTo relationship

--- app/Models/Stream.php
+++ app/Models/Stream.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\ClassForm;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Stream extends Model
